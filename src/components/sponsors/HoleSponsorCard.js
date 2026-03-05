5019f38ca4f0ab317ae370ebe80291d5153f6302
kristianlorgen <262289742+kristianlorgen@users.noreply.github.com>
2026-03-05 18:35:29 +0100

    Revert "Legg til sponsor-modul med feature toggle og hullsponsing"
    This reverts commit 64d688180000a05ec2f841f5422f4f17185b81a4.

--- src/components/sponsors/HoleSponsorCard.js
+++
@@ -1,6 +0,0 @@
-import { SponsorBadgeCard } from './SponsorBadgeCard';
-
-export function HoleSponsorCard({ holeNumber, sponsor, message = '' }) {
-  if (!sponsor || !holeNumber) return '';
-  return { holeNumber, card: SponsorBadgeCard({ sponsor, message }) };
-}
--- src/pages/admin/sponsors.js
+++
@@ -1,1 +0,0 @@
-export const adminSponsorsPageRoute = '/admin#sponsors';
