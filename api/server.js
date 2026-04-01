f418055bd9c9c05505c4a1d80d020e5cae5f7e61
kristianlorgen <262289742+kristianlorgen@users.noreply.github.com>
2026-04-01 23:32:57 +0200

    Fix Vercel API runtime with Express catch-all routing

--- api/server.js
+++ api/server.js
@@ -1,5 +1,1 @@
-module.exports = function handler(_req, res) {
-  res.statusCode = 500;
-  res.setHeader('content-type', 'application/json; charset=utf-8');
-  res.end(JSON.stringify({ error: 'Local storage not allowed in production' }));
-};
+module.exports = require('./index');
